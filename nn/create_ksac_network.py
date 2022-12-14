129455afca195acc10ad8a531ebee55d6fa21a09
prototyperesolution <86804949+prototyperesolution@users.noreply.github.com>
2022-12-14 21:30:25 +0000

    commit

--- nn/create_ksac_network.py
+++ nn/create_ksac_network.py
@@ -4,7 +4,7 @@ from tensorflow.keras import applications
 from nn.layers.ksac_block import KSAC_block
 import numpy as np
 from nn.layers.DeepLabV3_decoder import DeepLabV3_Decoder
-from utils import focal_loss
+from utils.losses import focal_loss
 
 class KSAC_network(tf.keras.Model):
     def __init__(self, input_shape, n_classes, filters, dilation_rate=[6,12,18], batchnorm=True):
@@ -17,16 +17,15 @@ class KSAC_network(tf.keras.Model):
             pooling=None,
             classes=1000,
         )
+        print(resnet_backbone.summary())
         resnet_backbone = tf.keras.Model(inputs=resnet_backbone.inputs,
                                          outputs=[resnet_backbone.get_layer('conv3_block4_out').output,
                                                   resnet_backbone.get_layer('conv4_block6_out').output])
-        #print(resnet_backbone.summary())
+
 
         x = tf.keras.Input(input_shape)
         x1, x2 = resnet_backbone(x)
-        print(x.shape)
         x2 = KSAC_block(filters, x2.shape, dilation_rate, batchnorm)(x2)
-        print(x.shape)
         logits = DeepLabV3_Decoder(filters, n_classes, input_shape[:-1])(x1,x2)
         self.model = tf.keras.Model(inputs=x, outputs=logits)
         self.loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
@@ -42,17 +41,13 @@ class KSAC_network(tf.keras.Model):
         with tf.GradientTape() as tape:
             logits = self.model(images, training=True)
             loss = tf.reduce_mean(focal_loss(masks, logits))
-            #loss = tf.nn.softmax_cross_entropy_with_logits(masks, logits, axis=-1, name=None)
-        #accuracy = np.mean(
-        #    np.argmax(logits.numpy(), -1) == masks)
         grads = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
         self.focal_loss_metric.update_state(loss)
-        #self.accuracy_metric.update_state(accuracy)
 
         return {
             'l1_loss': self.focal_loss_metric.result(),
             #'accuracy': self.accuracy_metric.result()
         }
 
-test_ksac = KSAC_network
+test_ksac = KSAC_network(input_shape=(256,256,3), n_classes=3, filters=50)
